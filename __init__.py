8d8173bf165ec6f964ed40fd8f96ceed75ab8408
marpot <marcin.potoczny20@gmail.com>
2020-04-24 15:55:01 +0200

    In progress

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import sqlite3
 from flask import Flask
 
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import hashlib
-
-from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+
+import flask_admin
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+
 app = Flask(__name__)
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin = Admin(app, name='LoginSystem', template_mode='bootstrap3')
+
+
+# add administrative views here
+class UserView(object):
+    class Post(object):
+        pass
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("login.html")
+
 
 def login():
     error = None
@@ -14,11 +30,12 @@ def login():
         username = request.form['username']
         password = request.form['password']
         completion = validate(username, password)
-        if completion == False:
+        if not completion:
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('secret'))
     return render_template('login.html', error=error)
+
 
 def validate(username, password):
     con = sqlite3.connect('static/user.db')
@@ -34,10 +51,10 @@ def validate(username, password):
                 completion = check_password(dbPass, password)
     return completion
 
+
 def check_password(hashed_password, user_password):
     return hashed_password == hashlib.md5(user_password.encode()).hexdigest()
 
 
 if __name__ == "__main__":
     app.run()
-
